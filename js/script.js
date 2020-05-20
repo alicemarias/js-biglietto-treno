d80d75a614a60b8096b8bb5b859ce3a93985930f
alice maria spina <alicem.spina93@gmail.com>
2020-05-20 20:43:59 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,24 @@
+//chiedo all'utente la sua eta
+var eta = parseInt(prompt('quanti anni hai?'))
+
+// chiedo all'untente quanti km deve viaggiare
+var km = parseInt(prompt('per quanti km dovrai viaggiare?'))
+
+//stabilisco prezzo pieno
+var prezzo = km * 0.21;
+
+// prezzo per over 65
+if (eta > 65) {
+  over65 = (prezzo * 40) / 100;
+  document.getElementById('biglietto').innerHTML = over65 + '£';
+}
+// prezzo per minorenni
+ else if (eta < 18) {
+  minorenni = (prezzo * 20) / 100;
+  document.getElementById('biglietto').innerHTML = minorenni + '£';
+}
+// prezzo pieno per tutti gli altri 
+else {
+  prezzo = km * 0.21;
+  document.getElementById('biglietto').innerHTML = prezzo + '£';
+}
